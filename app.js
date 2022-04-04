707dbdddd8c7ac5abf711d78e393a0823bf50d83
V11Playko <heinnervega20@gmail.com>
2022-04-04 16:50:31 -0500

    Agregando nodemon

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Ruta para hacer el / en el servidor
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
