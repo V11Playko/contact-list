973a02319240705feb51078955cacb64b57e15f8
V11Playko <heinnervega20@gmail.com>
2022-04-05 07:51:12 -0500

    Se hizo el css del proyecto con bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+//  Contactos
+const contactos =[
+  {
+    nombre: 'Heinner',
+    numero: '131231'
+  },
+  {
+    nombre: 'Javier',
+    numero: '131231'
+  },
+  {
+    nombre: 'Tom',
+    numero: '131231'
+  },
+  {
+    nombre: 'Duke',
+    numero: '134331'
+  },
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs');
+  res.render('index.ejs',{contactos});
 });
 
 module.exports = router;
